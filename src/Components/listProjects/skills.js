e840239ea004ca3cc5051aa566d0b1d99205e168
santiagogui1909 <santiagogui1909@gmail.com>
2022-03-15 19:50:34 -0500

    agrego nuevos proyecto al la data

--- src/Components/listProjects/skills.js
+++ src/Components/listProjects/skills.js
@@ -1,12 +1,14 @@
 const Skills = ({skillId}) => {
-    const skills = ["HTML" , "Css" , "Js" , "React"];
+    const skills = ["HTML" , "Css" , "Js" , "React", "Redux"];
 
     let skillUsed;
 
-    if (skillId === 1 || skillId === 3 || skillId === 4) {
-        skillUsed = skills.filter(value => value === "React" || value === "Css");
+    if (skillId === 1 || skillId === 2) {
+        skillUsed = skills.filter(value => value === "React" || value === "Redux");
+    } else if (skillId === 3 || skillId === 4) {
+        skillUsed = skills.filter(value => value === "React");
     } else {
-        skillUsed = skills.filter(value => value !== "React");
+        skillUsed = skills.filter(value => value !== "React" && value !== "Redux");
     }
 
     return (
